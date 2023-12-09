fc7e2b8be4dc023ba34127cdcbc48bf3f8ead892
KrisnaTriAnggara <110578223+KrisnaTriAnggara@users.noreply.github.com>
2023-12-09 14:58:14 +0700

    Add files via upload

---
+++ src/com/JavaProgram/ProgrammingFundamental.java
@@ -0,0 +1,22 @@
+package com.JavaProgram;
+
+public class ProgrammingFundamental {
+
+    public static void main(String[] args) {
+
+        for (int i=0; i<9; i++){
+            for (int j=0; j<9; j++){
+                System.out.print("* ");
+                if ( i == j ){
+                    break;
+                } else if ( (i+j) == 8 ) {
+                    break;
+                }
+            }
+            System.out.print("\n");
+        }
+        
+
+    }
+
+}
